3297d303a1347e3ff00497e0eca6597882fe6e00
jkewgns <wig24003978@heartofyorkshire.ac.uk>
2025-01-13 15:59:10 +0000

    Playground Testing

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Timer")]
+    public float timer;
+    public TMP_Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameTimer();
+    }
+    
+    void GameTimer()
+    {
+        timerText.text = timer.ToString("F3");
+
+        timer += Time.deltaTime;
+    }
+}
