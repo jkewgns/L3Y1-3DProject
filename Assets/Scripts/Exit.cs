d304e96d076ee8462f403535f813d587ced6c27b
jkewgns <wig24003978@heartofyorkshire.ac.uk>
2025-01-25 00:01:27 +0000

    Transitions + Scenery
    spent 2 - 3 hours stuck on code and all i had to do was write 2 words... i hate coroutines </3

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -1,15 +1,37 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour
 {
+    public Animator transition;
+    public float transitionTime = 1f;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            LoadNextLevel();
         }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //Play animation
+        transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load scene
+        SceneManager.LoadScene(levelIndex);
     }
 }
